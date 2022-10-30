fb838f4bbd7da052c3920c38a9753128643682a1
blackphantom0221 <blackphantom0221@gmail.com>
2022-10-30 18:16:24 -0400

    'init'

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_login import LoginManager
+from .models import User, db
+from config import Config
+from flask_migrate import Migrate
 
-
+from .auth.routes import auth
 app = Flask(__name__)
 login = LoginManager()
 
@@ -9,7 +12,17 @@ login = LoginManager()
 def load_user(user_id):
     return User.query.get(user_id)
     
-# Register Blueprint(AKA If we create another folder for login and stuff)
+app.config.from_object(Config)
+
+
+#register Blueprint
+app.register_blueprint(auth)
+
+# initialize our database to work with app
+db.init_app(app)
+migrate = Migrate(app, db)
+login.init_app(app)
 
 
 from . import routes 
+from . import models
